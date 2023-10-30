1f6b4f15c46b2d150cfe48e54fcfc0c7357aba1a
Uberlan Torres <uberlan9@gmail.com>
2023-10-30 18:58:25 -0300

    Exception handling - update

--- src/main/java/com/uberlanIFF/curso/Services/UserService.java
+++ src/main/java/com/uberlanIFF/curso/Services/UserService.java
@@ -13,6 +13,8 @@ import com.uberlanIFF.curso.Repositories.UserRepository;
 import com.uberlanIFF.curso.Services.Exceptions.DatabaseException;
 import com.uberlanIFF.curso.Services.Exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -42,9 +44,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
